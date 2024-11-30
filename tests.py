86fbc6b45e19133b5598d4467dd5e33c48f07f14
Guilherme Freschi <guilhermefreschix@gmail.com>
2024-11-30 13:58:37 -0300

    testing update and delete task

--- tests.py
+++ tests.py
@@ -33,3 +33,33 @@ def test_get_task():
     response_json = response.json()
     assert task_id == response_json['id']
 
+def test_update_task():
+  if tasks:
+    task_id = tasks[0]
+    payload = {
+      "completed": True,
+      "title": "Tarefa Atualizada",
+      "description": "Descrição Atualizada"
+    }
+    response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+
+    # Nova requisição na tarefa específica
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert task_id == response_json['id']
+    assert response_json["title"] == payload["title"]
+    assert response_json["description"] == payload["description"]
+    assert response_json["completed"] == payload["completed"]
+   
+def test_delete_task():
+  if tasks:
+    task_id = tasks[0]
+    response = response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+    
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 404 # precisa dar erro, já que foi excluído
