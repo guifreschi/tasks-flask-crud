373681712690f4bd0dd9c694b0895995bee7e1d8
Guilherme Freschi <guilhermefreschix@gmail.com>
2024-11-30 13:27:39 -0300

    testing read task

--- tests.py
+++ tests.py
@@ -12,12 +12,24 @@ def test_create_task():
   }
   response = requests.post(f"{BASE_URL}/tasks", json=new_task_data)
   assert response.status_code == 200 
-  # para testar o status da response precisa dar 200 (sucesso)
   response_json = response.json()
 
   assert "message" in response_json 
   assert "id" in response_json
-  # verifica se "message" e "id" estão na resposta
   tasks.append(response_json['id'])
 
-  
+def test_get_tasks():
+  response = requests.get(f"{BASE_URL}/tasks")
+  assert response.status_code == 200
+  response_json = response.json()
+  assert "tasks" in response_json
+  assert "total_tasks" in response_json
+
+def test_get_task():
+  if tasks:
+    task_id = tasks[0]
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert task_id == response_json['id']
+
